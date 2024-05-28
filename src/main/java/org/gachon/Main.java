ce311f227f47c52b36f1763b4778430b2912f667
PSJ1564 <psj15641@gmail.com>
2024-05-29 00:02:21 +0900

    Update Main.java
    예시직원 생성

--- src/main/java/org/gachon/Main.java
+++ src/main/java/org/gachon/Main.java
@@ -2,6 +2,25 @@ package org.gachon;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // 예시 직원 생성
+        Employee employee = Employee.builder()
+                .name("Kim gachon")
+                .phoneNumber("000-0000-0000")
+                .email("gc@gachon.ac.kr")
+                .salary(3000)
+                .department("Dev1")
+                .career("None")
+                .school("Gachon University")
+                .militaryService(true)
+                .rewardPunishment("No records")
+                .overWorkmin(0)
+                .vacation(17)
+                .build();
+        // EmployeeRepository 인스턴스 생성
+        EmployeeRepository repository = new NaiveEmployeeRepository();
+        // 직원 저장
+        repository.save(employee1);
+        
+     
     }
 }
