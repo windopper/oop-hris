c2945d03bfe4fc3071b622563cdc7fd5673bd194
dave04182 <168446217+dave04182@users.noreply.github.com>
2024-05-29 13:15:39 +0900

    Update EmployeeRepository.java

--- src/main/java/org/gachon/employee/EmployeeRepository.java
+++ src/main/java/org/gachon/employee/EmployeeRepository.java
@@ -11,11 +11,17 @@ public interface EmployeeRepository {
 
     public List<Employee> findByName(String name);
 
+    public List<Employee> findByCodeNumber(int codeNumber);
+
     public List<Employee> findByPhoneNumber(String phoneNumber);
 
     public List<Employee> findByEmail(String email);
 
     public List<Employee> findByDepartment(String department);
+
+    public List<Employee> findByYearOfEmployment(int yearOfEmployment);
+
+    public List<Employee> findByMarriage(boolean marriage);
 
     public List<Employee> findBySchool(String school);
 
